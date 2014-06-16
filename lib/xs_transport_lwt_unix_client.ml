948191b53c4057acb88615934fcf60478b71dc66
David Scott <dave.scott@citrix.com>
2014-06-16 21:20:16 +0100

    client: search likely paths for the xenstore socket
    We look for:
    
      XENSTORED_PATH (if set)
      the Unix domain socket
      /proc/xen/xenbus
      /dev/xen/xenstore
    
    Note we have to open the /proc file in a special way because it
    behaves more like a pipe or socket but isn't manifested as a pipe
    or socket in stat(2).
    
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- lib/xs_transport_lwt_unix_client.ml
+++ lib/xs_transport_lwt_unix_client.ml
@@ -17,19 +17,58 @@
 open Lwt
 
 (* Individual connections *)
-type channel = Lwt_unix.file_descr * Lwt_unix.sockaddr
+type channel = Lwt_unix.file_descr
+
+(* We'll look for these paths in order: *)
+let get_xenstore_paths () =
+  let default = [
+    !Xs_transport.xenstored_socket;
+    "/proc/xen/xenbus"; (* Linux *)
+    "/dev/xen/xenstore"; (* FreeBSD *)
+  ] in
+  try
+    Sys.getenv "XENSTORED_PATH" :: default
+  with Not_found -> default
+
+let choose_xenstore_path () =
+  List.fold_left (fun acc possibility -> match acc with
+    | Some x -> Some x
+    | None ->
+      if Sys.file_exists possibility then Some possibility else None
+  ) None (get_xenstore_paths ())
+
+exception Could_not_find_xenstore
+
 let create () =
-  let sockaddr = Lwt_unix.ADDR_UNIX(!Xs_transport.xenstored_socket) in
-  let fd = Lwt_unix.socket Lwt_unix.PF_UNIX Lwt_unix.SOCK_STREAM 0 in
-  lwt () = Lwt_unix.connect fd sockaddr in
-  return (fd, sockaddr)
-let destroy (fd, _) = Lwt_unix.close fd
-let read (fd, _) = Lwt_unix.read fd
-let write (fd, _) bufs ofs len =
-	lwt n = Lwt_unix.write fd bufs ofs len in
-	if n <> len then begin
-		fail End_of_file
-	end else return ()
+  ( match choose_xenstore_path () with
+    | None ->
+      Printf.fprintf stderr "Failed to find xenstore socket. I tried the following:\n";
+      List.iter (fun x -> Printf.fprintf stderr "  %s\n" x) (get_xenstore_paths ());
+      Printf.fprintf stderr "\nOn linux you might not have xenfs mounted:\n";
+      Printf.fprintf stderr "   sudo mount -t xenfs xenfs /proc/xen\n";
+      Printf.fprintf stderr "Or perhaps you just need to set the XENSTORED_PATH environment variable.\n";
+      fail Could_not_find_xenstore
+    | Some x -> return x ) >>= fun path ->
+  Lwt_unix.stat path >>= fun stats ->
+  match stats.Lwt_unix.st_kind with
+  | Lwt_unix.S_SOCK ->
+    let sockaddr = Lwt_unix.ADDR_UNIX(path) in
+    let fd = Lwt_unix.socket Lwt_unix.PF_UNIX Lwt_unix.SOCK_STREAM 0 in
+    Lwt_unix.connect fd sockaddr >>= fun () ->
+    return fd
+  | _ ->
+    let fd = Unix.openfile path [ Lwt_unix.O_RDWR ] 0o0 in
+    (* It looks like a file but behaves like a pipe: *)
+    return (Lwt_unix.of_unix_file_descr ~blocking:false fd)
+
+let destroy = Lwt_unix.close
+let read = Lwt_unix.read
+
+let write fd bufs ofs len =
+  Lwt_unix.write fd bufs ofs len >>= fun n ->
+  if n <> len
+  then fail End_of_file
+  else return ()
 
 type 'a t = 'a Lwt.t
 let return = Lwt.return
