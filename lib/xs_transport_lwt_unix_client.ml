1eeae9215b0ccac324c2878b8265575bb0412f3a
Yang Qian <yang.qian@citrix.com>
2018-05-21 17:21:10 +0800

    XSI-312 CA-289145: close socket if error occurs when using lwt connect
    Signed-off-by: Yang Qian <yang.qian@citrix.com>
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- lib/xs_transport_lwt_unix_client.ml
+++ lib/xs_transport_lwt_unix_client.ml
@@ -16,13 +16,19 @@
 
 open Lwt
 
+type 'a t = 'a Lwt.t
+let return = Lwt.return
+let ( >>= ) = Lwt.bind
+
 (* Individual connections *)
 type channel = Lwt_unix.file_descr * Lwt_unix.sockaddr
 let create () =
   let sockaddr = Lwt_unix.ADDR_UNIX(!Xs_transport.xenstored_socket) in
   let fd = Lwt_unix.socket Lwt_unix.PF_UNIX Lwt_unix.SOCK_STREAM 0 in
-  lwt () = Lwt_unix.connect fd sockaddr in
-  return (fd, sockaddr)
+    Lwt.catch
+      (fun () -> Lwt_unix.connect fd sockaddr)
+      (fun e  -> Lwt_unix.close fd >>= fun () -> Lwt.fail e)
+    >>= fun () -> return (fd, sockaddr)
 let destroy (fd, _) = Lwt_unix.close fd
 let read (fd, _) = Lwt_unix.read fd
 let write (fd, _) bufs ofs len =
@@ -30,10 +36,6 @@ let write (fd, _) bufs ofs len =
 	if n <> len then begin
 		fail End_of_file
 	end else return ()
-
-type 'a t = 'a Lwt.t
-let return = Lwt.return
-let ( >>= ) = Lwt.bind
 
 type backend = [`unix | `xen]
 let backend = `unix
