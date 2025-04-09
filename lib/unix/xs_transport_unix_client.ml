xapi-project/ocaml-xenstore-clients

commit b6d83e1a18c5b615be7a8676239700ad2d660ecc

Add backwards-compatibility ocamlfind subpackages
As part of the jbuilder converstion, the packages

  xenstore_transport
  xenstore_transport.unix
  xenstore_transport.lwt

were merged into `xenstore_transport`. This patch adds aliases to the
META file so we don't needlessly break the build of other components
which try to link with the subpackage.

This fixes the build of packages like vhd-tool, see
https://github.com/ocaml/opam-repository/pull/9481#issuecomment-307814311

Signed-off-by: David Scott <dave@recoil.org>

META.xenstore_transport.template
@@ -1,0 +1,11 @@
+# JBUILDER_GEN
+
+package "unix" (
+  description = "Deprecated. Use xenstore_transport"
+  requires = "xenstore_transport"
+)
+
+package "lwt" (
+  description = "Deprecated. Use xenstore_transport"
+  requires = "xenstore_transport"
+)

commit f86743a2bf5932dbd94d87d4dc71c2349e1a7f98

split Lwt and non-Lwt code into separate libraries
Introduce xenstore_transport.core and xenstore_transport.lwt.

Code that only wants the Unix backend doesn't need to link Lwt.
This is useful if applications introduce link-time checks to avoid
accidentally linking and using Lwt functions (e.g. such as XAPI, because it has many
blocking functions, and is incompatible with Lwt).

For backwards compatibility xenstore_transport is retained as a library
that links and reexports both core and lwt, so users can move to use the
'.core' library at the own pace.

This effectively undoes the deprecation in, turns out the separate
subpackages would be useful after all:
Fixes: b6d83e1a1 ("Add backwards-compatibility ocamlfind subpackages")

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

META.xenstore_transport.template
@@ -1,11 +1,0 @@
-# JBUILDER_GEN
-
-package "unix" (
-  description = "Deprecated. Use xenstore_transport"
-  requires = "xenstore_transport"
-)
-
-package "lwt" (
-  description = "Deprecated. Use xenstore_transport"
-  requires = "xenstore_transport"
-)

dune-project
@@ -1,2 +1,3 @@
-(lang dune 1.0)
+(lang dune 2.0)
+
 (name xenstore-tool)

lib/dune
@@ -1,6 +1,8 @@
 (library
  (name xenstore_transport)
  (public_name xenstore_transport)
- (libraries lwt xenstore xenstore.client lwt.unix)
+ (libraries
+  (re_export xenstore_transport.unix)
+  (re_export xenstore_transport.lwt))
  (wrapped false)
  (flags :standard -safe-string))

lib/lwt/dune
@@ -1,0 +1,7 @@
+(library
+ (name xenstore_transport_lwt)
+ (public_name xenstore_transport.lwt)
+ (libraries lwt xenstore xenstore.client lwt.unix xenstore_transport.unix)
+ (wrapped false)
+ (flags
+  (:standard -safe-string)))

lib/unix/dune
@@ -1,0 +1,6 @@
+(library
+ (name xenstore_transport_core)
+ (public_name xenstore_transport.unix)
+ (libraries unix)
+ (wrapped false)
+ (flags :standard -safe-string))

lib/unix/xs_transport_unix_client.ml
@@ -40,7 +40,7 @@ let create () =
         raise e
     end
   | _ ->
-    Unix.openfile path [Lwt_unix.O_RDWR] 0o0
+    Unix.openfile path [Unix.O_RDWR] 0o0
 
 let destroy fd = Unix.close fd
 let read fd = Unix.read fd